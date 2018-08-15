1691d2f1c6be8277e1c25ce896d2d96c7923a5fa
roltel <roltel@roltel.net>
2018-08-15 02:30:59 +0100

    Primeiro commit com o conteudo que vem do gitlab. Alojamento de procução em futuragora.pt

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Civil Cultural</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script src="js/main.js"></script>
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        body {
+            background-color: bisque;
+        }
+
+        div {
+
+            text-align: center;
+        }
+
+        #nav,
+        #nav {
+            height: 75px;
+        }
+
+        #nav>[class*='col-'] {
+            padding-top: 15px;
+        }
+
+
+
+        #space {
+            height: 50px;
+        }
+
+        #content .row {
+            min-height: 150px;
+        }
+
+        #sidebar {
+            padding-top: 40px
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-md-2" id="sidebar">
+                <h4>
+                    <a href="#">Return to main</a>
+                </h4>
+                <h4>Portal News</h4>
+            </div>
+            <div class="col-md-10">
+                <div class="row" id="nav">
+                    <div class="col-sm-2">
+                        <a href="#">Change language</a>
+                    </div>
+                    <div class="col-sm-8">
+                        <span>
+                            <img src="images/civilcultural.jpg" alt="logo">CIVIL CULTURAL</span>
+                    </div>
+                    <div class="col-sm-2">
+                        <span>United Kingdom
+                            <img src="images/English-Language-Flag-1-icon.png" height="50" alt="flag">
+                        </span>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="container" id="content">
+
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                        </div>
+
+                        <span id="space">&nbsp; </span>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                            <div class="col-md">
+                                <a href="https://placeholder.com">
+                                    <img class="img-fluid img-thumbnail" src="http://via.placeholder.com/350x150">
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
